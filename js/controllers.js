6c2f39643d9a3a6d74c2beeaeb93fc84b6cf68e4
James Thompson <qbi11y@gmail.com>
2015-12-09 10:35:46 -0600

    added ability to add application

--- js/controllers.js
+++ js/controllers.js
@@ -9,22 +9,28 @@ app.controller('IndexCtrl',['$scope', 'Applications', function($scope, Applicati
         app.id = Math.floor(Math.random() * 100);
         Applications.addApp(app);
         $scope.applications = Applications.getApps();
-        console.log($scope.applications)
     }
     console.log('index');
 }]);
 
 app.controller('ApplicationCtrl', ['$scope', '$state', '$stateParams', 'Applications', function($scope, $state, $stateParams, Applications) {
-    console.log(Applications.getApp($stateParams.id));
+    //console.log(Applications.getApp($stateParams.id));
     $scope.currentStep = 1;
     $scope.application = Applications.getApp($stateParams.id);
     $scope.showEnviro = function(enviro) {
-        console.log(enviro)
         Applications.setEnviro(enviro)
         $state.go('environment')
+    }
+    $scope.createEnvironment = function(enviro) {
+        Applications.addEnviro($stateParams.id, enviro);
+    }
+    $scope.removeEnvironment = function(enviro) {
+        console.log('enviro to remove ', enviro);
+        Applications.removeEnviro($stateParams.id, enviro);
     }
 }]);
 
 app.controller('EnviroCtrl', ['$scope', '$stateParams', 'Applications', function($scope, $stateParams, Applications) {
     console.log(Applications.getEnviro())
+    $scope.environment = Applications.getEnviro();
 }])
--- js/data.js
+++ js/data.js
@@ -7,38 +7,47 @@ app.factory('Applications', function() {
         name: 'BI Hadoop with Docker',
         description: 'Business Intelligence profile that defines infrastructure requirements for web, application and database server',
         environments: [{
-            name: 'Enviro 1',
-            resources: {
-                network: {
-                    bandwidth: 3,
-                    inbound: 3,
-                    outbound: 4,
-                    qty: 4
-                },
-                servers: {
-                    cores: 1,
-                    cycles: 4,
-                    localDisk: 3,
-                    mappingFactor: 4,
-                    os: 'Linux',
-                    qty: 4,
-                    ram: 2,
-                    usage: {
-                        daily: 3,
-                        monthly: 4,
-                        total: 4
-                    }
-                },
-                storage: {
-                    qty: 100,
-                    cloud: 3,
-                    network: 4,
-                    shared: 5
+            name: 'Enviro 1',            
+            network: [{
+                bandwidth: 3,
+                inbound: 3,
+                outbound: 4,
+                qty: 4
+            }],
+            servers: [{
+                cores: 1,
+                cycles: 4,
+                localDisk: 3,
+                mappingFactor: 4,
+                os: 'Linux',
+                qty: 4,
+                ram: 2,
+                usage: {
+                    daily: 3,
+                    monthly: 4,
+                    total: 4
                 }
-            }
+            }],
+            storage: [{
+                qty: 100,
+                cloud: 3,
+                network: 4,
+                shared: 5
+            }]
+            
         }]
 
     }];
+
+    function buildResource(resource, resources) {
+        console.log(resource, resources);
+        var tmpResources = [];
+        for (var n=0; n < resources.qty; n++) {            
+            tmpResources.push(resources)
+        }
+        return tmpResources
+
+    }
     
     return {
         getApps: function() {
@@ -54,7 +63,49 @@ app.factory('Applications', function() {
         },
 
         addApp: function(d) {
-            applications.push(d);
+            console.log('application to breakdown ', d);
+            var resources = {};
+            var application = {
+                id: d.id,
+                name: d.name,
+                description: d.description,
+                environments: []
+            } 
+
+            for (obj in d) {
+                if (obj == 'network' || obj == 'servers' || obj == 'storage') {
+                    resources[obj] = buildResource(obj, d[obj]);                    
+                }
+            }
+
+            for (var n=0; n < d.environments; n++) {
+                application.environments.push(resources);
+                application.environments[n].name = 'Environment ' + n;
+            }
+
+            applications.push(application);
+            console.log('applications ', applications);                    
+        },
+
+        addEnviro: function(appID, d) {
+            console.log('create enviro ', d);
+            var environment = {
+                name: d.name,
+            }
+            var resources = {};
+            for (obj in d) {
+                if (obj == 'network' || obj == 'servers' || obj == 'storage') {
+                    environment[obj] = buildResource(obj, d[obj]);                    
+                }
+            }
+
+            for (var n=0; n < applications.length; n++) {
+                if (applications[n].id == appID) {
+                    applications[n].environments.push(environment);
+                    break;
+                }
+            }
+            console.log('all applications ', applications);
         },
 
         setEnviro: function(d) {
@@ -63,6 +114,18 @@ app.factory('Applications', function() {
 
         getEnviro: function() {
             return enviro
+        },
+
+        removeEnviro: function(appID, d) {
+            for (var n=0; n < applications.length;  n++) {
+                if (applications[n].id == appID) {
+                    for (var i=0; i < applications[n].environments.length; i++) {
+                        if (applications[n].environments[i] == d) {
+                            applications[n].environments.splice(i, 1);
+                        }
+                    }
+                }
+            }
         }
     }
 })
